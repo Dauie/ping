0612ad7c3563f426dc98c401ea98d29cfa969159
Robert LUTT <ausdauerr@gmail.com>
2018-07-18 00:48:36 -0700

    changes to checksum

--- src/checksum.c
+++ src/checksum.c
@@ -1,18 +1,16 @@
 #include "../incl/ping.h"
 
-u_int16_t checksum(void *buff, size_t nwords)
+u_int16_t checksum(void *data, size_t nwords)
 {
-	u_int8_t	*data;
 	u_int16_t	word;
 	u_int32_t	accum;
 	size_t		i;
 
-	data = (uint8_t *)buff;
 	accum = 0xffff;
 	i = 0;
 	while (i < nwords)
 	{
-		ft_memcpy(&word, data + i, 2);
+		ft_memcpy(&word, (uint8_t *)data + i, 2);
 		accum += ntohs(word);
 		if (accum > 0xffff)
 			accum -= 0xffff;
@@ -21,9 +19,10 @@ u_int16_t checksum(void *buff, size_t nwords)
 	if (nwords == 1)
 	{
 		word = 0;
-		ft_memcpy(&word, data + nwords - 1, 1);
+		ft_memcpy(&word, (uint8_t *)data + nwords - 1, 1);
 		accum += ntohs(word);
 		if (accum > 0xffff)
 			accum -= 0xffff;
 	}
+	return (ntohs(~accum));
 }
