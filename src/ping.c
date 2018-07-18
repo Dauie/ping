226e3a9cacce4a2d21cc0935fa470f093f10255d
Robert LUTT <ausdauerr@gmail.com>
2018-07-18 16:47:31 -0700

    testing pingloop now

--- src/ping.c
+++ src/ping.c
@@ -1,21 +1,24 @@
+#include <netinet/ip_icmp.h>
 #include "../incl/ping.h"
 
-static void			fill_icmp_header(t_mgr *mgr, t_echo *msg, struct icmp *icmp, u_short datalen)
+static void				fill_icmp_header(t_mgr *mgr, t_echo *echo,
+											struct icmp *icmp)
 {
 	icmp->icmp_type = ICMP_ECHO;
 	icmp->icmp_code = 0;
 	icmp->icmp_cksum = 0;
 	icmp->icmp_hun.ih_idseq.icd_id = htons(mgr->pid);
 	icmp->icmp_hun.ih_idseq.icd_seq = htons(mgr->seq);
-	icmp->icmp_cksum = checksum((u_int8_t *)msg + IPV4_HDRLEN, ICMP_HDRLEN + datalen);
+	icmp->icmp_cksum = checksum((u_int8_t *)echo + IPV4_HDRLEN,
+								ICMP_HDRLEN + echo->datalen);
 }
 
-static void			fill_ip_header(t_mgr *mgr, struct ip *ip, int datalen)
+static void				fill_ip_header(t_mgr *mgr, struct ip *ip, t_echo *echo)
 {
 	ip->ip_hl = IPV4_HDRLEN / sizeof(uint32_t);
 	ip->ip_v = 4;
 	ip->ip_tos = 0;
-	ip->ip_len = htons(IPV4_HDRLEN + ICMP_HDRLEN + datalen);
+	ip->ip_len = htons(IPV4_HDRLEN + ICMP_HDRLEN + echo->datalen);
 	ip->ip_id = htons(0);
 	ip->ip_off = htons(0);
 	ip->ip_ttl = 255;
@@ -32,24 +35,40 @@ static void			fill_ip_header(t_mgr *mgr, struct ip *ip, int datalen)
 	}
 }
 
+void					prep_sockaddr(struct sockaddr_in *sin, t_echo *echo)
+{
+	ft_memset(sin, 0, sizeof (struct sockaddr_in));
+	sin->sin_family = AF_INET;
+	sin->sin_addr.s_addr = echo->ip.ip_dst.s_addr;
+}
+
+int 					ping_loop(t_mgr *mgr, t_echo *echo, struct sockaddr_in *sin)
+{
+	while (mgr->count)
+	{
+		if (sendto(mgr->sock, &echo, IPV4_HDRLEN + ICMP_HDRLEN + echo->datalen, 0,
+				   (struct sockaddr *)&sin, sizeof(struct sockaddr)) < 0)
+		{
+			dprintf(STDERR_FILENO, "Error sendto(). %s\n", strerror(errno));
+			exit(FAILURE);
+		}
+		if (mgr->flags.count == TRUE)
+			mgr->count -= 1;
+		echo->icmp.icmp_hun.ih_idseq.icd_seq += 1;
+	}
+	return (SUCCESS);
+}
+
 int						ping(t_mgr *mgr)
 {
 	struct sockaddr_in	sin;
 	t_echo				echo;
-	u_short				datalen;
 
-	ft_strcpy(echo.data, "FT ECHO REQUEST 424");
-	datalen = (u_short)ft_strlen(echo.data);
-	fill_ip_header(mgr, &echo.ip, datalen);
-	fill_icmp_header(mgr, &echo, &echo.icmp, datalen);
-	ft_memset(&sin, 0, sizeof (struct sockaddr_in));
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = echo.ip.ip_dst.s_addr;
-	if (sendto(mgr->sock, &echo, IPV4_HDRLEN + ICMP_HDRLEN + datalen, 0,
-		   (struct sockaddr *)&sin, sizeof(struct sockaddr)) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error sendto(). %s\n", strerror(errno));
-		exit(FAILURE);
-	}
+	ft_strcpy(echo.data, "FT ECHO REQUEST 42");
+	echo.datalen = (u_short)ft_strlen(echo.data);
+	fill_ip_header(mgr, &echo.ip, &echo);
+	fill_icmp_header(mgr, &echo, &echo.icmp);
+	prep_sockaddr(&sin, &echo);
+	ping_loop(mgr, &echo, &sin);
 	return (SUCCESS);
 }
--- src/struct_init.c
+++ src/struct_init.c
@@ -10,7 +10,7 @@ void		init_mgr(t_mgr *mgr)
 	mgr->seq = 1;
 	mgr->uid = getuid();
 	mgr->sock = 0;
-	mgr->count = 0;
+	mgr->count = 1;
 	ft_memset(mgr->domain, 0, DOMAIN_NAME_LEN);
 	ft_memset(mgr->saddr, 0, IPV4_ADDR_LEN);
 	ft_memset(mgr->daddr, 0, IPV4_ADDR_LEN);
