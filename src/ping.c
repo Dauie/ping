d4391cdb94a702e77f974fb973d57c15a6286601
Robert LUTT <ausdauerr@gmail.com>
2018-07-18 15:00:08 -0700

    working with checksum still

--- src/ping.c
+++ src/ping.c
@@ -4,6 +4,7 @@ static void			fill_icmp_header(t_mgr *mgr, t_echo *msg, struct icmp *icmp, u_sho
 {
 	icmp->icmp_type = ICMP_ECHO;
 	icmp->icmp_code = 0;
+	icmp->icmp_cksum = 0;
 	icmp->icmp_hun.ih_idseq.icd_id = htons(mgr->pid);
 	icmp->icmp_hun.ih_idseq.icd_seq = htons(mgr->seq);
 	icmp->icmp_cksum = checksum((u_int8_t *)msg + IPV4_HDRLEN, ICMP_HDRLEN + datalen);
@@ -35,14 +36,18 @@ int						ping(t_mgr *mgr)
 {
 	struct sockaddr_in	sin;
 	t_echo				echo;
-
 	u_short				datalen;
+//	u_int8_t			packet[IP_MAXPACKET];
 
 	ft_strcpy(echo.data, "FT ECHO REQUEST");
 	datalen = (u_short)ft_strlen(echo.data);
 	fill_ip_header(mgr, &echo.ip, datalen);
 	fill_icmp_header(mgr, &echo, &echo.icmp, datalen);
-	ft_memset(&sin, 0, sizeof (struct sockaddr_in));
+//	ft_memset(packet, 0, IP_MAXPACKET);
+//	ft_memcpy(packet, &echo.ip, IPV4_HDRLEN);
+//	ft_memcpy(packet + IPV4_HDRLEN, &echo.icmp, ICMP_HDRLEN);
+//	ft_memcpy(packet + IPV4_HDRLEN + ICMP_HDRLEN, &echo.data, datalen);
+//	ft_memset(&sin, 0, sizeof (struct sockaddr_in));
 	sin.sin_family = AF_INET;
 	sin.sin_addr.s_addr = echo.ip.ip_dst.s_addr;
 	if (sendto(mgr->sock, &echo, IPV4_HDRLEN + ICMP_HDRLEN + datalen, 0,
