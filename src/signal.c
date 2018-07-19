021e30fb8e7296181a2b20ac82c84f8010f31c60
Robert LUTT <ausdauerr@gmail.com>
2018-07-19 15:20:54 -0700

    testing sighandeler

--- src/signal.c
+++ src/signal.c
@@ -4,8 +4,6 @@ void	alarm_handel_timeout(int sig)
 {
 	if (sig != SIGALRM)
 		return ;
-	signal(SIGALRM, SIG_IGN);
-	signal(SIGALRM, alarm_handel_timeout);
 	printf("Request timeout for icmp_seg %zu\n", g_mgr->seq);
 	ping_loop(g_mgr, &g_mgr->echo, &g_mgr->sin);
 }
