6e9c3564f9022020d579a83fb700ec147ffd8509
Robert LUTT <ausdauerr@gmail.com>
2018-07-29 21:45:50 -0700

    removing unneeded zero'ing

--- src/struct_init.c
+++ src/struct_init.c
@@ -11,9 +11,4 @@ void		init_mgr(t_mgr *mgr)
 	mgr->sock = 0;
 	mgr->count = 1;
 	mgr->seq = 1;
-	ft_memset(mgr->domain, 0, DOMAIN_NAME_LEN);
-	ft_memset(mgr->saddr, 0, IPV4_ADDR_LEN);
-	ft_memset(mgr->daddr, 0, IPV4_ADDR_LEN);
-	ft_memset(&mgr->stats, 0, sizeof(t_stats));
-	ft_memset(&mgr->flags, 0, sizeof(t_flags));
 }
