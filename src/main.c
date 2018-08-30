acc3b38efa0ee1f236fb78374dfea7a5138afff8
Robert LUTT <ausdauerr@gmail.com>
2018-08-30 12:55:31 -0700

    silly silly silly.

--- src/main.c
+++ src/main.c
@@ -23,11 +23,7 @@ static void				useage(void)
 
 static void				init_mgr(t_mgr *mgr)
 {
-	if ((mgr->pid = getpid()) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error getpid().\n");
-		exit(FAILURE);
-	}
+	mgr->pid = getpid();
 	mgr->uid = getuid();
 	mgr->sock = 0;
 	mgr->count = 1;
